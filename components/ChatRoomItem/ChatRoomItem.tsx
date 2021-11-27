f378a0c49693530dcc4e00641fee253679dbc674
Alexei Semenenko <aleksei.syemyenenko@gmail.com>
2021-11-27 21:11:39 +0300

    Updated chatRoomItem component and adjusted it

---
+++ components/ChatRoomItem/ChatRoomItem.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Image, Text, View } from 'react-native'
+
+import styles from './styles'
+import { ChatRoom } from './types'
+
+type Props = {
+  chatRoom: ChatRoom
+}
+
+function ChatRoomItem( props: Props): JSX.Element {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{ uri: props.chatRoom.users[1].imageUri }}
+        style={styles.image}
+      />
+
+      { props.chatRoom.newMessages && (
+        <View style={styles.badgeContainer}>
+          <Text style={styles.badgeNumber}>
+            {props.chatRoom.newMessages}
+          </Text>
+        </View>
+      )}
+
+      <View style={styles.secondLevelContainer}>
+        <View style={styles.row}>
+          <Text style={styles.name}>
+            {props.chatRoom.users[1].name}
+          </Text>
+
+          <Text style={styles.text}>
+            {props.chatRoom.lastMessage.createdAt}
+          </Text>
+        </View>
+
+        <Text numberOfLines={1} style={styles.text}>{props.chatRoom.lastMessage.content}</Text>
+      </View>
+    </View>
+  )
+}
+
+export default ChatRoomItem
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,33 +1,25 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import ChatRoomItem from '../components/ChatRoomItem'
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
-  );
-}
+import chatRoomData from '../assets/dummy-data/ChatRooms'
 
 const styles = StyleSheet.create({
-  container: {
+  page: {
+    backgroundColor: 'white',
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+})
+
+function TabOneScreen() {
+  return (
+    <View style={styles.page}>
+      <ChatRoomItem chatRoom={chatRoomData[0]} />
+      <ChatRoomItem chatRoom={chatRoomData[1]} />
+    </View>
+
+  )
+}
+
+export default TabOneScreen
