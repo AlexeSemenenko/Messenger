57be6b90f3213cf79f1f727d8689a0460367e412
Alexei Semenenko <aleksei.syemyenenko@gmail.com>
2021-11-27 21:25:17 +0300

    Added all items

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, FlatList } from 'react-native'
 
 import ChatRoomItem from '../components/ChatRoomItem'
-
-import chatRoomData from '../assets/dummy-data/ChatRooms'
+import chatRoomsData from '../assets/dummy-data/ChatRooms'
 
 const styles = StyleSheet.create({
   page: {
@@ -15,8 +14,12 @@ const styles = StyleSheet.create({
 function TabOneScreen() {
   return (
     <View style={styles.page}>
-      <ChatRoomItem chatRoom={chatRoomData[0]} />
-      <ChatRoomItem chatRoom={chatRoomData[1]} />
+      <FlatList
+        data={chatRoomsData}
+        renderItem={({ item }) => (
+          <ChatRoomItem chatRoom={item} />
+        )}
+      />
     </View>
 
   )
