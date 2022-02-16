1f0c826226ade1dab581b5dac7cbe8357a1dff97
Alexei Semenenko <aleksei.syemyenenko@gmail.com>
2022-02-16 20:22:54 +0300

    added auth

--- App.tsx
+++ App.tsx
@@ -1,23 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import Amplify from 'aws-amplify'
+// @ts-ignore
+import { withAuthenticator } from 'aws-amplify-react-native'
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from './hooks/useCachedResources'
+import useColorScheme from './hooks/useColorScheme'
+import Navigation from './navigation'
+import config from './src/aws-exports'
 
-export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+Amplify.configure(config)
+
+function App() {
+  const isLoadingComplete = useCachedResources()
+  const colorScheme = useColorScheme()
 
   if (!isLoadingComplete) {
-    return null;
+    return null
   } else {
     return (
       <SafeAreaProvider>
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
       </SafeAreaProvider>
-    );
+    )
   }
 }
+
+export default withAuthenticator(App)
